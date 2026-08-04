fd0287a9a733caa00644c143e9dd1de8a136fb3f
Elissar Fadel <fadel.elissar@gmail.com>
2026-08-04 05:53:10 -0400

    Changed layout

--- app/frontend/src/components/layout/Layout.tsx
+++ app/frontend/src/components/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
 import "./Layout.css";
 
 export default function Layout() {
@@ -8,13 +7,9 @@ export default function Layout() {
     <div className="app-layout">
       <Navbar />
 
-      <div className="app-body">
-        <Sidebar />
-
-        <main className="main-content">
-          <Outlet />
-        </main>
-      </div>
+      <main className="main-content">
+        <Outlet />
+      </main>
     </div>
   );
 }
